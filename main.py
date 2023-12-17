c502a1bdfcf830edebb23dd56e319ca71a07f1c6
DuranDurannn <144664669+DuranDurannn@users.noreply.github.com>
2023-12-17 10:34:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main_menu():
       option = int(input("\nPlease selct an option: "))
 
       if (option == 1):
-        GPT.main_output()
+        GPT.launch()
       elif (option == 2):
         option_menu()
       elif (option == 3):
